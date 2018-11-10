0ae5ff56ab4404eb1b0bc84894b8dbb8a399735f
Travis CI User <travis@example.org>
2018-11-10 02:13:57 +0000

    Update documentation

---
+++ implementors/futures/stream/trait.Stream.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rusoto_s3"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/futures/0.1/futures/stream/trait.Stream.html\" title=\"trait futures::stream::Stream\">Stream</a> for <a class=\"struct\" href=\"rusoto_s3/struct.StreamingBody.html\" title=\"struct rusoto_s3::StreamingBody\">StreamingBody</a>",synthetic:false,types:["rusoto_s3::generated::StreamingBody"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
